39e6fbc16a424e0980a6f9720279754b6f6891c2
lucasberenger <thegreatberenger@gmail.com>
2026-03-16 20:58:53 -0300

    fix base project

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/projeto-infracidade/',
   server: {
     host: "::",
     port: 8080,
-    base: '/projeto-infracidade/',
     hmr: {
       overlay: false,
     },
